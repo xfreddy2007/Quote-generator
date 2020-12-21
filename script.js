687bef615f46478e7e6abd84b38ad2138d3e5dc0
xfreddy2007 <freddy@appledeMacBook-Pro.local>
2020-12-21 20:04:18 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const nweQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+// Get Quote From API
+async function getQuoteFromAPI() {
+    showLoadingSpinner();
+    // We need to use a Proxy URL to  make our API call in order to avoid
+    const proxyUrl = 'https://shrouded-mountain-82883.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // If author is blank, add 'Unkonwn'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        // Stop Loader, Show Quote
+        removeLoadingSpinner();
+    } catch (error) {
+        getQuoteFromAPI();
+        console.log('whoops, no quote', error)
+    }
+}
+
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+nweQuoteBtn.addEventListener('click', getQuoteFromAPI);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuoteFromAPI();
